3be0d32246cadc9e32185560b964a859132fc796
Darren Morrison <darren09@live.ca>
2022-01-17 23:40:34 +0000

    Handle POST requests

--- myApp.js
+++ myApp.js
@@ -1,22 +1,27 @@
 require('dotenv').config()
+require('body-parser')
+
+var bodyParser = require('body-parser');
+
 
 var express = require('express');
 var app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 })
-
 app.use("/public", express.static(__dirname + "/public"));
 app.use(function middleware(req, res, next) {
   var string = req.method + " " + req.path + " - " + req.ip;
   console.log(string);
   next();
 });
-
 app.get('/:word/echo', function(req, res) {
   res.send({echo: req.params.word});
 });
-
 app.get("/name", function(req, res) {
   var firstName = req.query.first;
   var lastName = req.query.last;
@@ -24,14 +29,12 @@ app.get("/name", function(req, res) {
     name: `${firstName} ${lastName}`
   });
 });
-
 app.get('/now', function middleware(req, res, next) {
   req.time = new Date().toString();
   next();
 }, function(req, res) {
   res.send({time: req.time});
 });
-
 app.get('/json', function (req, res) {
   if(process.env.MESSAGE_STYLE == 'uppercase') {
     res.json({"message": "HELLO JSON"})
