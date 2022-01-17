de4eb8f6c638a6481434d302afd86461c0eb2c6c
Darren Morrison <darren09@live.ca>
2022-01-17 23:59:06 +0000

    Created a Post request handler

--- myApp.js
+++ myApp.js
@@ -9,19 +9,30 @@ var app = express();
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.post('/name', function (req, res) {
+  var firstName = req.body.first;
+  var lastName = req.body.last;
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+})
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 })
+;
 app.use("/public", express.static(__dirname + "/public"));
+
 app.use(function middleware(req, res, next) {
   var string = req.method + " " + req.path + " - " + req.ip;
   console.log(string);
   next();
 });
+
 app.get('/:word/echo', function(req, res) {
   res.send({echo: req.params.word});
 });
+
 app.get("/name", function(req, res) {
   var firstName = req.query.first;
   var lastName = req.query.last;
@@ -29,19 +40,21 @@ app.get("/name", function(req, res) {
     name: `${firstName} ${lastName}`
   });
 });
+
 app.get('/now', function middleware(req, res, next) {
   req.time = new Date().toString();
   next();
 }, function(req, res) {
   res.send({time: req.time});
 });
+
 app.get('/json', function (req, res) {
   if(process.env.MESSAGE_STYLE == 'uppercase') {
     res.json({"message": "HELLO JSON"})
   } else {
     res.json({"message": "Hello json"})
   }
-})
+});
 
 
 
